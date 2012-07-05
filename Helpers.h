e188f290505abd5aa93dbda80aec9361f0b7596b
Kent Nguyen <nguyen.dmz@gmail.com>
2012-07-05 23:48:25 +0800

    Release code for public

---
+++ Helpers.h
@@ -0,0 +1,10 @@
+#import <CoreFoundation/CoreFoundation.h>
+#import <QuartzCore/CAAnimation.h>
+
+extern BOOL IsDeviceIPad();
+extern BOOL IsDeviceIPad2();
+
+extern BOOL IsOS5();
+
+extern NSString * deviceModel();
+extern CATransition * fadeTransition();
---
+++ Helpers.m
@@ -0,0 +1,38 @@
+#import "Helpers.h"
+#import <sys/utsname.h>
+#import <QuartzCore/QuartzCore.h>
+
+BOOL IsDeviceIPad() {
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 30200
+  if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+    return YES;
+  }
+#endif
+  return NO;
+}
+
+BOOL IsDeviceIPad2() {
+  return (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad && [UIImagePickerController isSourceTypeAvailable: UIImagePickerControllerSourceTypeCamera]);
+}
+
+BOOL IsOS5() {
+  return [[[UIDevice currentDevice] systemVersion] floatValue] >= 5.00;
+}
+
+NSString * deviceModel() {
+  struct utsname systemInfo;
+  uname(&systemInfo);
+
+  return [NSString stringWithCString:systemInfo.machine
+                            encoding:NSUTF8StringEncoding];
+}
+
+CATransition * fadeTransition() {
+  CATransition* transition = [CATransition animation];
+  transition.duration = 0.8;
+  transition.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+  transition.type = kCATransitionFade; //kCATransitionMoveIn; //, kCATransitionPush, kCATransitionReveal, kCATransitionFade
+                                       //transition.subtype = kCATransitionFromTop; //kCATransitionFromLeft, kCATransitionFromRight, kCATransitionFromTop, kCATransitionFromBottom
+  return transition;
+}
+
