6f4bdbc723ae73802a9f7f2e7082c598ead55d5a
Kent Nguyen <nguyen.dmz@gmail.com>
2012-10-06 12:08:42 +0800

    Add UIImage category supporting iPhone 5

---
+++ UIImage+Retina4.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+Retina4.h
+//  StunOMatic
+//
+//  Created by Benjamin Stahlhood on 9/12/12.
+//  Copyright (c) 2012 DS Media Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Retina4)
+
+@end
